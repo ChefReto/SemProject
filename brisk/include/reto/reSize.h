3584ff3045e053d33754da05282e783c6fb20371
RetoMichael <michaere@student.ethz.ch>
2017-08-07 09:13:39 +0200

    Removed folder from repository

--- brisk/include/reto/reSize.h
+++
@@ -1,7 +0,0 @@
-cv::Mat reSize(cv::Mat image){
-	const int kNewWidth = 256;
-	const int kNewHeight = 256;
-	cv::Mat new_image;
-	resize(image, new_image, cvSize(kNewWidth, kNewHeight));
-	return new_image;
-}
--- brisk/include/reto/sortalgorithm.h
+++
@@ -1,8 +0,0 @@
-//Sort algorithm
-
-#include <algorithm>
-using namespace std;
-
-void sort1(vector<double> &data, vector<double> &v){
- 	std::sort(v.begin(),v.end());
-}
